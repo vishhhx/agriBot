2163e8e5966056f6073923f33406814f4c0586ac
vish <pastevishwa@gmail.com>
2026-09-06 03:11:57 +0530

    changed the next config

--- frotend/next.config.ts
+++ frotend/next.config.ts
@@ -1,11 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: [
+    "ec2-3-6-221-61.ap-south-1.compute.amazonaws.com",
+  ],
+
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: process.env.BACKEND_URL || "http://localhost:5000/api/:path*",
+        destination:
+          process.env.BACKEND_URL || "http://localhost:5000/api/:path*",
       },
     ];
   },
